c4125855d9224b876934037f9aa509956d41f2ef
Anderson Barros <41713400+andersonbarrosfrontend@users.noreply.github.com>
2022-08-18 12:32:38 -0300

    ajuste no DB

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { MongoClient } = require("mongodb");
 
 // const url = "mongodb://localhost:27017";
-const url = "mongodb+srv://admin:BL2i3zGjM8jskm93@cluster0.tldedbt.mongodb.net";
+const url = "mongodb+srv://admin:1p8PrB38UbfDlDhM@cluster0.keecddn.mongodb.net";
 const dbName = "jornada-fullstack-agosto-22";
 
 
